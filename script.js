1cde620fe733748ad368955c215ec744598559d4
matiasportugal <mportugal818@gmail.com>
2026-01-27 17:46:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showSection(id) {
-  document.querySelectorAll('.section').forEach(section => {
-    section.classList.remove('active');
+  document.querySelectorAll('.section').forEach(s => {
+    s.classList.remove('active');
   });
 
   document.getElementById(id).classList.add('active');
